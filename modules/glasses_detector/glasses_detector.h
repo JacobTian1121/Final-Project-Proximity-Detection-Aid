7b4645c903a7b683c3a752d5fa07184627abcb3c
JacobTian1121 <125104221+JacobTian1121@users.noreply.github.com>
2023-03-11 16:42:19 -0500

    Create glasses_detector.h

---
+++ modules/glasses_detector/glasses_detector.h
@@ -0,0 +1,19 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _GLASSES_DETECTOR_H_
+#define _GLASSES_DETECTOR_H_
+
+//=====[Declaration of public defines]=========================================
+
+#define SYSTEM_TIME_INCREMENT_MS   60
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void GlassesDetectorInit();
+void GlassesDetectorUpdate();
+
+//=====[#include guards - end]=================================================
+
+#endif // _GLASSES_DETECTOR_H_
